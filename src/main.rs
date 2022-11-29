241e2e66deec57484e70c0b54d502662e636ca45
Nagy Tibor <xnagytibor@protonmail.com>
2022-11-29 16:48:16 +0100

    Find a crate that can uncompress Qt's qCompress

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,9 @@
+use miniz_oxide::inflate::decompress_to_vec_zlib;
+use std::str;
+
 fn main() {
+    let test_bin = include_bytes!("../test/test.mmpz");
+
+    let decompress_result = decompress_to_vec_zlib(&test_bin[4..]);
+    println!("{}", str::from_utf8(&decompress_result.unwrap()).unwrap());
 }
