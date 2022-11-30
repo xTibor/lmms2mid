e7b9b1ea66a9a563e35c99d31582ed6a6bb9b975
Nagy Tibor <xnagytibor@protonmail.com>
2022-11-30 15:26:23 +0100

    Convert note data

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use std::collections::HashMap;
 use std::path::PathBuf;
 
 mod lmms_model;
-use lmms_model::LmmsProject;
+use lmms_model::{LmmsProject, LMMS_TICKS_PER_BAR};
 
 use clap::Parser;
 use midly::num::{u15, u24, u28, u4, u7};
@@ -9,9 +10,12 @@ use midly::{
     Format, Header, MetaMessage, MidiMessage, Smf, Timing, Track, TrackEvent, TrackEventKind,
 };
 
-const MIDI_CC_BANK_SELECT: u8 = 0;
+const MIDI_CC_BANK_SELECT_COARSE: u8 = 0;
+const MIDI_CC_BANK_SELECT_FINE: u8 = 32;
 const MIDI_CC_VOLUME: u8 = 7;
 const MIDI_CC_PANNING: u8 = 10;
+
+const MIDI_MAX_POLYPHONY: usize = 24;
 
 /// A less broken MIDI-exporter for LMMS
 #[derive(Debug, Parser)]
@@ -22,10 +26,6 @@ struct Args {
 
     /// Output MIDI file (.mid)
     output_path: PathBuf,
-
-    /// Ticks per beat
-    #[arg(short, long, default_value_t = 576)]
-    ticks_per_beat: usize,
 
     /// Track name
     #[arg(long)]
@@ -51,6 +51,34 @@ pub struct AbsoluteTrackEvent<'a> {
 
     /// MIDI event data
     pub kind: TrackEventKind<'a>,
+}
+
+pub trait TrackEventKindExt {
+    fn is_note_on(&self) -> bool;
+
+    fn is_note_off(&self) -> bool;
+}
+
+impl TrackEventKindExt for TrackEventKind<'_> {
+    fn is_note_on(&self) -> bool {
+        matches!(
+            self,
+            TrackEventKind::Midi {
+                message: MidiMessage::NoteOn { .. },
+                ..
+            }
+        )
+    }
+
+    fn is_note_off(&self) -> bool {
+        matches!(
+            self,
+            TrackEventKind::Midi {
+                message: MidiMessage::NoteOff { .. },
+                ..
+            }
+        )
+    }
 }
 
 fn main() {
@@ -112,7 +140,7 @@ fn main() {
 
     let mut midi_document = Smf::new(Header::new(
         Format::SingleTrack,
-        Timing::Metrical(u15::from(args.ticks_per_beat as u16)),
+        Timing::Metrical(u15::from((LMMS_TICKS_PER_BAR / 4) as u16)),
     ));
 
     let mut midi_track = Track::new();
@@ -147,10 +175,10 @@ fn main() {
 
     // MIDI channel initialization
 
-    for (midi_channel, lmms_track) in lmms_track_midi_channel {
+    for (midi_channel, lmms_track) in &lmms_track_midi_channel {
         midi_track.push(TrackEvent {
             delta: u28::from(0),
-            kind: TrackEventKind::Meta(MetaMessage::MidiChannel(midi_channel)),
+            kind: TrackEventKind::Meta(MetaMessage::MidiChannel(*midi_channel)),
         });
 
         if !lmms_track.name.is_empty() {
@@ -168,37 +196,53 @@ fn main() {
             });
         }
 
-        // Bank selection
-        midi_track.push(TrackEvent {
-            delta: u28::from(0),
-            kind: TrackEventKind::Midi {
-                channel: midi_channel,
-                message: MidiMessage::Controller {
-                    controller: u7::from(MIDI_CC_BANK_SELECT),
-                    value: u7::from(lmms_track.sf2_player().bank as u8),
-                },
-            },
-        });
+        // Bank and preset selection
+        {
+            let bank = lmms_track.sf2_player().bank;
+            let bank_coarse = u7::from((bank >> 7) as u8);
+            let bank_fine = u7::from((bank & 0x7F) as u8);
 
-        // Preset selection
-        midi_track.push(TrackEvent {
-            delta: u28::from(0),
-            kind: TrackEventKind::Midi {
-                channel: midi_channel,
-                message: MidiMessage::ProgramChange {
-                    program: u7::from(lmms_track.sf2_player().patch as u8),
+            midi_track.push(TrackEvent {
+                delta: u28::from(0),
+                kind: TrackEventKind::Midi {
+                    channel: *midi_channel,
+                    message: MidiMessage::Controller {
+                        controller: u7::from(MIDI_CC_BANK_SELECT_COARSE),
+                        value: bank_coarse,
+                    },
                 },
-            },
-        });
+            });
+
+            midi_track.push(TrackEvent {
+                delta: u28::from(0),
+                kind: TrackEventKind::Midi {
+                    channel: *midi_channel,
+                    message: MidiMessage::Controller {
+                        controller: u7::from(MIDI_CC_BANK_SELECT_FINE),
+                        value: bank_fine,
+                    },
+                },
+            });
+
+            midi_track.push(TrackEvent {
+                delta: u28::from(0),
+                kind: TrackEventKind::Midi {
+                    channel: *midi_channel,
+                    message: MidiMessage::ProgramChange {
+                        program: u7::from(lmms_track.sf2_player().patch as u8),
+                    },
+                },
+            });
+        }
 
         // Volume
         midi_track.push(TrackEvent {
             delta: u28::from(0),
             kind: TrackEventKind::Midi {
-                channel: midi_channel,
+                channel: *midi_channel,
                 message: MidiMessage::Controller {
                     controller: u7::from(MIDI_CC_VOLUME),
-                    value: u7::from(0), // TODO: remap
+                    value: u7::from(127), // TODO: remap
                 },
             },
         });
@@ -207,16 +251,142 @@ fn main() {
         midi_track.push(TrackEvent {
             delta: u28::from(0),
             kind: TrackEventKind::Midi {
-                channel: midi_channel,
+                channel: *midi_channel,
                 message: MidiMessage::Controller {
                     controller: u7::from(MIDI_CC_PANNING),
-                    value: u7::from(0), // TODO: remap
+                    value: u7::from(64), // TODO: remap
                 },
             },
         });
     }
 
-    //let mut midi_track_events = Vec::new();
+    let mut midi_track_events = Vec::new();
+
+    for (midi_channel, lmms_track) in &lmms_track_midi_channel {
+        for lmms_pattern in &lmms_track.patterns {
+            for lmms_note in &lmms_pattern.notes {
+                let ticks_start = lmms_pattern.position + lmms_note.position;
+                let ticks_end = ticks_start + lmms_note.length;
+
+                midi_track_events.push(AbsoluteTrackEvent {
+                    ticks: ticks_start,
+                    ticks_event_start: ticks_start,
+                    kind: TrackEventKind::Midi {
+                        channel: *midi_channel,
+                        message: MidiMessage::NoteOn {
+                            key: u7::from(lmms_note.key as u8),
+                            vel: u7::from(127u8), // TODO: remap
+                        },
+                    },
+                });
+
+                midi_track_events.push(AbsoluteTrackEvent {
+                    ticks: ticks_end,
+                    ticks_event_start: ticks_start,
+                    kind: TrackEventKind::Midi {
+                        channel: *midi_channel,
+                        message: MidiMessage::NoteOff {
+                            key: u7::from(lmms_note.key as u8),
+                            vel: u7::from(127u8), // TODO: remap
+                        },
+                    },
+                });
+            }
+        }
+    }
+
+    midi_track_events.sort_by_key(
+        |&AbsoluteTrackEvent {
+             ticks,
+             ticks_event_start,
+             kind,
+             ..
+         }| {
+            (
+                ticks,
+                ticks_event_start,
+                !kind.is_note_on(),
+                !kind.is_note_off(),
+            )
+        },
+    );
+
+    {
+        let mut current_polyphony = 0;
+        let mut already_warned = false;
+
+        for event in midi_track_events.iter() {
+            if event.kind.is_note_on() {
+                current_polyphony += 1;
+
+                if (current_polyphony > MIDI_MAX_POLYPHONY) && !already_warned {
+                    eprintln!("warning: excessive polyphony at {}", event.ticks);
+                    already_warned = true;
+                }
+            }
+
+            if event.kind.is_note_off() {
+                assert!(current_polyphony > 0);
+                current_polyphony -= 1;
+
+                if (current_polyphony <= MIDI_MAX_POLYPHONY) && already_warned {
+                    already_warned = false;
+                }
+            }
+        }
+    }
+
+    {
+        let mut current_note_counts = HashMap::new();
+
+        for event in midi_track_events.iter() {
+            if let TrackEventKind::Midi {
+                channel,
+                message: MidiMessage::NoteOn { key, .. },
+            } = event.kind
+            {
+                let note_count = current_note_counts.entry((channel, key)).or_insert(0);
+                *note_count += 1;
+
+                if *note_count >= 2 {
+                    eprintln!("warning: note overlap at {}", event.ticks);
+                }
+            }
+
+            if let TrackEventKind::Midi {
+                channel,
+                message: MidiMessage::NoteOff { key, .. },
+            } = event.kind
+            {
+                let note_count = current_note_counts
+                    .get_mut(&(channel, key))
+                    .expect("failed to get note count");
+
+                assert!(*note_count > 0);
+                *note_count -= 1;
+
+                if *note_count == 0 {
+                    current_note_counts.remove(&(channel, key));
+                }
+            }
+        }
+    }
+
+    for (event_index, event) in midi_track_events.iter().enumerate() {
+        let delta_time = if event_index == 0 {
+            event.ticks
+        } else {
+            let ticks_before = midi_track_events[event_index - 1].ticks;
+            let ticks_current = midi_track_events[event_index].ticks;
+            assert!(ticks_before <= ticks_current);
+            ticks_current - ticks_before
+        };
+
+        midi_track.push(TrackEvent {
+            delta: u28::from(delta_time as u32),
+            kind: event.kind,
+        });
+    }
 
     midi_track.push(TrackEvent {
         delta: u28::from(0),
